717cc68846d2ded8b6fde3afac008eddaed8adb2
Nahzilian <nahz3112@gmail.com>
2022-04-20 16:30:47 -0400

    Update ROC curve options

--- ROC.py
+++ ROC.py
@@ -1,10 +1,9 @@
 from matplotlib import pyplot
 from pandas import DataFrame
 from helpers.preprocess import preprocess
-from helpers.confusion_matrix import cm_converter
-from helpers.merge_sort import merge_sort
+from helpers.confusion_matrix import cm_converter, cm_converter_custom_threshold
+# from helpers.merge_sort import merge_sort
 # import matplotlib.pyplot as plt
-
 
 class ROC:
     def __init__(self, pos_val, neg_val, value_col, class_col, data: DataFrame, plt: pyplot, sort_by: list[str] = [], r_col: list[str] = [], thresholds: str = '', label: str = '', nan_val = None, i_col:str= '') -> None:
@@ -33,12 +32,11 @@ class ROC:
         # print(self.tpr)
         # print(self.fpr)
 
-    # def calc_accuracy_with_threshold(self, threshold):
-    #     # Where to get the index????
-    #     cm = cm_converter(self.data, threshold, self.pos_val,self.neg_val, self.value_col, self.class_col, index + 1, self.i_col)
-    #     pass
+    def calc_measurements_with_threshold(self, threshold):
+        # Where to get the index????
+        cm = cm_converter_custom_threshold(self.data, threshold, self.pos_val,self.neg_val, self.value_col, self.class_col)
+        return cm.get_measurements()
 
-        
 
     def plot_curves(self):
         
--- helpers/confusion_matrix.py
+++ helpers/confusion_matrix.py
@@ -1,6 +1,5 @@
 from pandas import DataFrame
 
-
 class ConfusionMatrix:
     def __init__(self, tp, fp, tn, fn) -> None:
         self.tp = tp
@@ -9,17 +8,25 @@ class ConfusionMatrix:
         self.fp = fp
         self.tpr = self.tp / (self.tp + self.fn)
         self.fpr = self.fp / (self.fp + self.tn)
-    
+
     def get_precision(self):
         return self.tp / (self.tp + self.fp)
 
-    def get_recal(self):
+    def get_recall(self):
         return self.tp / (self.tp + self.fn)
-    
+
     def get_f_measure(self):
         return 2 * self.tp / (2 * self.tp + self.fp + self.fn)
 
-def cm_converter (data: DataFrame, threshold, pos_val, neg_val, col, class_col, index, index_col):
+    def get_measurements(self):
+        return {
+            "precision": self.get_precision(),
+            "recall": self.get_recall(),
+            "f_measure": self.get_f_measure()
+        }
+
+
+def cm_converter(data: DataFrame, threshold, pos_val, neg_val, col, class_col, index, index_col):
     '''
     tp: all above value that match positive with threshold
     fp: all above expected to match positive but appear negative in dataframe
@@ -27,15 +34,15 @@ def cm_converter (data: DataFrame, threshold, pos_val, neg_val, col, class_col, 
     fn: all below expected match negative but appear positive in dataframe
     '''
 
-    positive_rows = data[col] >= threshold
-    negative_rows = data[col] < threshold
-    
-    
     # assuming that data has been sorted and index is also correct
-    is_tp = data[(data[col] >= threshold) & (data[class_col] == pos_val) & (data[index_col] <= index)]
-    is_tn = data[(data[col] <= threshold) & (data[class_col] == neg_val) & (data[index_col] > index)]
-    is_fp = data[(data[col] >= threshold) & (data[class_col] == neg_val) & (data[index_col] <= index)]
-    is_fn = data[(data[col] <= threshold) & (data[class_col] == pos_val)  & (data[index_col] > index)]
+    is_tp = data[(data[col] >= threshold) & (
+        data[class_col] == pos_val) & (data[index_col] <= index)]
+    is_tn = data[(data[col] <= threshold) & (
+        data[class_col] == neg_val) & (data[index_col] > index)]
+    is_fp = data[(data[col] >= threshold) & (
+        data[class_col] == neg_val) & (data[index_col] <= index)]
+    is_fn = data[(data[col] <= threshold) & (
+        data[class_col] == pos_val) & (data[index_col] > index)]
 
     # tp, fp, tn, fn = 3 3 2 2
     # tp, fp, tn, fn = 4 4 1 1 --- 0.85 -- id 6
@@ -44,3 +51,15 @@ def cm_converter (data: DataFrame, threshold, pos_val, neg_val, col, class_col, 
     # print(tp, fp, tn, fn)
     return ConfusionMatrix(tp, fp, tn, fn)
 
+
+def cm_converter_custom_threshold(data: DataFrame, threshold, pos_val, neg_val, col, class_col):
+    # Assume that threshold value is not in the table
+
+    is_tp = data[(data[col] >= threshold) & (data[class_col] == pos_val)]
+    is_tn = data[(data[col] <= threshold) & (data[class_col] == neg_val)]
+    is_fp = data[(data[col] >= threshold) & (data[class_col] == neg_val)]
+    is_fn = data[(data[col] <= threshold) & (data[class_col] == pos_val)]
+
+    tp, fp, tn, fn = is_tp.shape[0], is_fp.shape[0], is_tn.shape[0], is_fn.shape[0]
+
+    return ConfusionMatrix(tp, fp, tn, fn)
